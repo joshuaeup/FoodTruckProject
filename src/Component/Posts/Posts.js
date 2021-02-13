59d86c56851fe3a8080c34c510ddd46a2f87630b
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-12 22:05:06 -0500

    Added form

--- src/Component/Posts/Posts.js
+++ src/Component/Posts/Posts.js
@@ -7,16 +7,89 @@ class Posts extends Component {
         this.state = { posts: PostsData };
     }
 
-    addPost = () => {};
+    addPost = (e) => {
+        // Prevents page from reloading
+        e.preventDefault();
+
+        // Name from input
+        let name = document.forms["add"]["name"].value;
+
+        // Address input with plus sign between spaces
+        let address = document.forms["add"]["address"].value
+            .split(" ")
+            .join("+");
+        // Description input
+        let description = document.forms["add"]["description"].value;
+
+        console.log("ALL VALUES", name, address, description);
+
+        // Arr Copy to be joined with main state
+        let arrCopy = this.state.posts;
+
+        arrCopy.unshift({
+            name: name,
+            description: description,
+            photo:
+                "https://nmgprod.s3.amazonaws.com/media/files/60/48/6048b1e4d0b20d40eea243534785ed3f/cover_image_1582904889.jpg.760x400_q85_crop_upscale.jpg",
+            link: `https://www.google.com/maps/place/${address}`,
+        });
+
+        console.log("Array copy", arrCopy);
+
+        this.setState({
+            posts: arrCopy,
+        });
+        // Empty fields
+        document.forms["add"]["name"].value = "";
+        address = document.forms["add"]["address"].value = "";
+        document.forms["add"]["description"].value = "";
+    };
 
     render() {
         return (
             <>
-                <div className="posts-main-container">
-                    {/* <form onSubmit={this.addPost}>
-                        <input placeholder="Add Post" />
-                        <input type="submit" />
-                    </form> */}
+                <div id="posts-main-container">
+                    <h2 style={{ textAlign: "center", marginBottom: "2rem" }}>
+                        Add New Truck
+                    </h2>
+                    <form id="form" name="add" onSubmit={this.addPost}>
+                        <div className="form-row ">
+                            <div className="form-group col-md-6">
+                                <label htmlFor="name">Food Truck Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="name"
+                                    placeholder="Crusty Crab"
+                                />
+                            </div>
+                            <div className="form-group col-md-6">
+                                <label htmlFor="address">Address</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="address"
+                                    placeholder="1234 Main St"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-row">
+                            <div className="form-group col-md-12">
+                                <label htmlFor="description">Description</label>
+                                <textarea
+                                    className="form-control"
+                                    id="description"
+                                    rows="3"
+                                    placeholder="Add Description Here"
+                                ></textarea>
+                            </div>
+                        </div>
+
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </form>
                     {this.state.posts.map((post, index) => {
                         return (
                             <div className="posts-container" key={index}>
@@ -27,8 +100,13 @@ class Posts extends Component {
                                     <p className="posts-body">
                                         {post.description}
                                     </p>
-                                    <a className="posts-link" href={post.link}>
-                                        {post.linkText}
+                                    <a
+                                        className="posts-link"
+                                        href={post.link}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Click for directions
                                     </a>
                                 </section>
                                 <section className="posts-container__image">
--- src/Component/Posts/PostsData.js
+++ src/Component/Posts/PostsData.js
@@ -7,7 +7,6 @@ const PostsData = [
             "https://nmgprod.s3.amazonaws.com/media/files/60/48/6048b1e4d0b20d40eea243534785ed3f/cover_image_1582904889.jpg.760x400_q85_crop_upscale.jpg",
         link:
             "https://www.google.com/maps/search/?api=1&query=35.305173000,-80.724091000",
-        linkText: "Click for directions",
     },
     {
         name: "Taza",
@@ -17,7 +16,6 @@ const PostsData = [
             "https://nmgprod.s3.amazonaws.com/media/files/60/48/6048b1e4d0b20d40eea243534785ed3f/cover_image_1582904889.jpg.760x400_q85_crop_upscale.jpg",
         link:
             "https://www.google.com/maps/search/?api=1&query=35.329210000,-80.738825000",
-        linkText: "Click for directions",
     },
     {
         name: "Papi Queso",
@@ -27,8 +25,17 @@ const PostsData = [
             "https://nmgprod.s3.amazonaws.com/media/files/60/48/6048b1e4d0b20d40eea243534785ed3f/cover_image_1582904889.jpg.760x400_q85_crop_upscale.jpg",
         link:
             "https://www.google.com/maps/search/?api=1&query=35.234320000,-80.827372000",
-        linkText: "Click for directions",
+    },
+    {
+        name: "Papi Queso",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        photo:
+            "https://nmgprod.s3.amazonaws.com/media/files/60/48/6048b1e4d0b20d40eea243534785ed3f/cover_image_1582904889.jpg.760x400_q85_crop_upscale.jpg",
+        link: "https://www.google.com/maps/place/3325+daniel+place+drive",
     },
 ];
+
+// https://www.google.com/maps/place/3325+daniel+place+drive
 
 export default PostsData;
