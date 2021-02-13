0484a5cbafd1d0be4a62557ef4009275009c6609
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-12 20:40:13 -0500

    Added front end

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import "./App.css";
 import React, { Component } from "react";
+import Posts from "./Component/Posts/Posts";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+    constructor() {
+        super();
+        this.state = {};
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="banner">
+                    <h1>Website Title</h1>
+                </div>
+                <Posts />
+                <div className="banner">
+                    <h1>Website Footer</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
